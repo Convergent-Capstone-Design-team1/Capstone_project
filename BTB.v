7c55ae2306aa4219b602ec618b6ec2dcf3b930c0
GyeongMin Kim <kim4j@naver.com>
2023-03-27 00:52:02 +0900

    0327 first

--- BTB.v
+++ BTB.v
@@ -16,32 +16,56 @@ module BTB
   output  [33:0]  next_pc       // 다음 명령어 주소
 );
 
-  reg [ENTRY_WIDTH-1:0] btb [NUM_ENTRIES-1:0]; // BTB 메모리
+  
+  /******************* for simulation *************************/
+  
+  generate
+    genvar  idx;
+    for (idx = 0; idx < 64; idx = idx+1) begin: branch_target
+       wire [63:0] tmp;
+       assign tmp = btb[idx];
+    end
+  endgenerate
 
-  integer i; // 반복문을 위한 변수
+  integer i; 
   initial begin
       for (i = 0; i < 64; i = i+1)
         btb[i] = 64'b0;
   end
 
-  reg     [33:0]  next_pc_r;
+  /********************** module start *************************/
 
-  
+  reg [ENTRY_WIDTH-1:0] btb [NUM_ENTRIES-1:0];              // BTB 메모리
+  reg [33:0]  next_pc_r = 34'b0;
+  reg pend = 1'b0;
+  reg [1:0] cnt = 2'b00;
+
   always @(*) begin
     if(branch) begin
-      for (i = 0; i < NUM_ENTRIES; i = i + 1) begin
-        if (btb[i][63:32] == pc[31:0]) begin // 현재 명령어 주소와 일치하는 엔트리를 찾으면
-          if (taken == 2'b10 || taken == 2'b11) begin // 분기를 예측한 경우
-            next_pc_r = btb[i][31:0]; // 분기 목적지 주소로 점프
+      next_pc_r = target;                                   //104로 들어가게 해야함 
+      if (btb[pc[9:2]][63:34] == pc[31:2]) begin            // 테이블에서 지금 pc를 발견함
+        pend = 1'b0;
+        if (taken == 2'b10 || taken == 2'b11) begin         // 분기를 예측한 경우
+            next_pc_r = btb[pc[9:2]][31:0];                 // 분기 목적지 주소로 점프
           end
-
-          btb[i] <= {target[31:0], taken}; // BTB 엔트리 업데이트
-          i = i + NUM_ENTRIES; // 반복문 종료
-        end
+        btb[pc[9:2]] <= {pc[31:2], taken, target[31:0]};  // BTB 엔트리 업데이트
+      end
+      else begin
+        pend = 1'b1;
+      end
+    end
+    else if(pend) begin
+      if(cnt == 2'b11) begin
+        next_pc_r = target;
+        pend = 1'b0;
+        cnt = 2'b00;
+      end
+      else begin
+        cnt = cnt + 1'b1;
       end
     end
   end
-
+    
   assign next_pc = next_pc_r;
 
 endmodule
